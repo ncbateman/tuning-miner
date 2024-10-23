cc9bab79720f2f10296eac5bc3d68c3873d1f633
Nick Bateman <ncbateman@gmail.com>
2024-10-23 17:13:51 +0000

    typo fix

--- axolotl/endpoints/training.py
+++ axolotl/endpoints/training.py
@@ -1,14 +1,13 @@
 import os
 import subprocess
 import redis
+import yaml
 from loguru import logger
 from fastapi import APIRouter, HTTPException
 
-from utilities.dataset import download_dataset
-
 from models.training_request import TrainingRequest
 from models.miner_task_request import MinerTaskRequest
-from models.file_format import FileFormat
+from models.miner_task_response import MinerTaskResponse
 
 from config.config_handler import create_config
 
@@ -27,7 +26,7 @@ def set_training_flag():
 def clear_training_flag():
     r.delete(TRAINING_FLAG_KEY)
 
-@router.post("/train")
+@router.post("/start_training")
 async def start_training(request: TrainingRequest):
     if is_training():
         raise HTTPException(status_code=409, detail="Training already in progress. Please wait for the current job to finish.")
@@ -68,7 +67,8 @@ async def task_offer(request: MinerTaskRequest) -> MinerTaskResponse:
 async def get_latest_model_submission(task_id: str) -> str:
     try:
         config_filename = f"{task_id}.yml"
-        config_path = os.path.join(cst.CONFIG_DIR, config_filename)
+
+        config_path = os.path.join('/workspace/config/', config_filename)
         with open(config_path, "r") as file:
             config_data = yaml.safe_load(file)
             return config_data.get("hub_model_id", None)
@@ -76,20 +76,3 @@ async def get_latest_model_submission(task_id: str) -> str:
     except Exception as e:
         logger.error(f"Error retrieving latest model submission for task {task_id}: {str(e)}")
         raise HTTPException(status_code=404, detail=f"No model submission found for task {task_id}")
-
-router.add_api_route(
-    "/start_training/",
-    start_training,
-    tags=["Subnet"],
-    methods=["POST"],
-)
-
-router.add_api_route(
-    "/get_latest_model_submission/{task_id}",
-    get_latest_model_submission,
-    tags=["Subnet"],
-    methods=["GET"],
-    response_model=str,
-    summary="Get Latest Model Submission",
-    description="Retrieve the latest model submission for a given task ID",
-)
